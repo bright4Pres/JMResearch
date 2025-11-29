ae9e8578e6864e2bf15240b27d70baab1339698f
bright <knowncarbon@gmail.com>
2025-11-29 12:29:54 +0800

    UI for home_screen

---
+++ myapp/lib/screens/home_screen.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 70,
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () {
+              // Handle menu tap
+            },
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/profile.jpg'),
+              radius: 20,
+            ),
+          ),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Iskaon',
+              style: TextStyle(
+                color: Colors.deepOrange,
+                fontFamily: 'Roboto',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Order now, pick up later',
+              style: TextStyle(
+                fontSize: 14,
+                color: const Color.fromARGB(255, 125, 116, 38),
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            color: Colors.blueGrey,
+            onPressed: () {
+              // Handle notification tap
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  padding: EdgeInsets.zero,
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  minimumSize: const Size(330, 180),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/Meals.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  width: 330,
+                  height: 180,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 20,
+                        left: 20,
+                        child: Container(
+                          width: 100,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 255, 255, 35),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Text(
+                            "Campus Canteen",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        left: 20,
+                        child: Container(
+                          width: 200,
+                          height: 35,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Full Meals",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                "Red Plate x Silver Plate x No Queue!",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 9,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  padding: EdgeInsets.zero,
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  shape: RoundedRectangleBorder(),
+                  minimumSize: const Size(330, 180),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/Snacks.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  width: 330,
+                  height: 180,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 20,
+                        left: 20,
+                        child: Container(
+                          width: 100,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 255, 255, 35),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Text(
+                            "Campus Vendors",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        left: 20,
+                        child: Container(
+                          width: 200,
+                          height: 35,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Snacks & Treats",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                "Judy's x Lopez's x KD's Churro's!",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 9,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
